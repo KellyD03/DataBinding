861ba222aca9f8c14f9418279cf440391e7b5749
Kelly Monserrat Chi Duarte <24470031@campeche.tecnm.mx>
2025-10-29 15:06:05 -0600

    Ejemplo de sincronizacion de elementeos XAML con manejo de eventos

---
+++ DataBinding2.XamlElement/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DataBinding2.XamlElement
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
